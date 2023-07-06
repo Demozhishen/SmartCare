b59e0c2d6751e3ceb6ae00d4f9b8fddb160d4a45
cheng <1448527427@qq.com>
2023-07-06 11:15:09 +0800

    智慧养老信息维护后端

--- src/test/java/com/bjtu/web/spring_boot/ApplicationTests.java
+++ src/test/java/com/bjtu/web/spring_boot/ApplicationTests.java
@@ -12,17 +12,7 @@ class ApplicationTests {
 
 
 
-        @Autowired
-        private StaffMapper stusMapper;
 
-
-        @Test
-        public void testSelect () {
-            List<Staff> Staff = stusMapper.selectList(null);
-            for (Staff staff : Staff) {
-                System.out.println(Staff);
-            }
-        }
 
     }
 
